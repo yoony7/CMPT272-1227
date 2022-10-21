d2fd257aa4255bd07e21a536bec2528782475dd3
yoony7 <91112885+yoony7@users.noreply.github.com>
2022-10-21 01:18:14 -0700

    Add files via upload

--- Assignment 2/calculate.js
+++ Assignment 2/calculate.js
@@ -1,75 +1,60 @@
 var bounds = [100, 95.0, 90.0, 85.0, 80.0, 75.0, 70.0, 65.0, 60.0, 55.0, 50.0, 0.0]; // default bound values
 var fileUploaded = false;
+var objects = [];
 
 const myBounds = document.getElementById("formbounds"); // messy extraction of bound input text 
-myBounds.addEventListener("input", function () {
+
+function nanError() {
+    window.location.reload();
+}
+
+function boundUpdate() {
     if (fileUploaded == true) {
         for (var i = 1; i < 13; i++) {
             var j = "text" + i;
-            bounds[i - 1] = (document.getElementById(j).value);
+            var k = document.getElementById(j).value;
+            bounds[i - 1] = Number(k);
+            console.log(Number.isNaN(bounds[i - 1]));
+            if ((bounds[i - 1]) !== (bounds[i - 1])) {
+                console.log("string detected");
+                console.log(bounds[i - 1]);
+                console.log(Number.isNaN(bounds[i - 1]));
+                document.getElementById("boundError").textContent = "Error: please only enter numbers. Refreshing in 5 seconds."
+                setTimeout(nanError, 5000);
+                throw new Error("NaN input");
+            }
         }
-        console.log(bounds);
+        function compare(a, b) {
+            if (Number(a) > Number(b)) { return -1; }
+            if (Number(a) < Number(b)) { return 1; }
+            return 0;
+        }
+        bounds.sort(compare); // sort bounds
     }
-})
 
-function init() {
-    document.getElementById("selectedFile").addEventListener('change', handleFileSelect, false);
+    return bounds;
 }
 
-function handleFileSelect(event) {
-    const reader = new FileReader()
-    reader.onload = handleFileLoad;
-    reader.readAsText(event.target.files[0])
-}
-
-function handleFileLoad(event) {
-    console.log(event);
-    console.log("Submission logged");
-    fileUploaded = true;
-    document.getElementById("fileContent").textContent = event.target.result;
-    const text = event.target.result;
-    const data = csvToArray(text);
-
-    function compare(a, b) { // Compare function for sort()
-        if (Number(a.Percent) > Number(b.Percent)) {
-            return -1;
-        }
-        if (Number(a.Percent) < Number(b.Percent)) {
-            return 1;
-        }
-        return 0;
-    }
-    data.sort(compare); // sort data
-    console.log(data);
-
-    var min = data[0];
+function calcStats(data) {
+    var max = data[0];
     var maxIndex = 0;
-    var max = data[data.length - 1];
+    var min = data[data.length - 1];
     var minIndex = data.length - 1;
 
-    for (var i = 1; i < 13; i++) {
-        var j = "text" + i;
-        bounds[i - 1] = (document.getElementById(j).value);
-        console.log(bounds[i - 1]);
-    }
-
-    console.log(Number(data[0].Percent) > Number(bounds[0]));
+    var bounds = boundUpdate();
+    console.log(bounds);
 
     for (var i = 0; i < data.length; ++i) {
-        if (Number(data[i].Percent) > Number(bounds[0])) {
+        if (Number(data[i].Percent) > (bounds[0])) {
             max = data[i + 1];
-            console.log("before increment", maxIndex, i);
             ++maxIndex;
-            console.log("after increment", maxIndex, i);
         }
     }
 
     for (var i = data.length; i > 0; --i) {
-        if (Number(data[i - 1].Percent) < Number(bounds[11])) {
+        if (Number(data[i - 1].Percent) < (bounds[11])) {
             min = data[i - 2];
-            console.log("before increment", minIndex, i);
             --minIndex;
-            console.log("after increment", minIndex, i);
         }
     }
     console.log(max);
@@ -95,11 +80,77 @@ function handleFileLoad(event) {
 
     var median = data[Math.ceil(sliced.length / 2) - 1].Percent;
     document.getElementById("median").textContent = median;
+    objects = sliced;
 }
 
+function calcGrades(data) {
+    var aplus = 0, a = 0, aminus = 0, bplus = 0, b = 0, bminus = 0, cplus = 0, c = 0, cminus = 0, d = 0, f = 0;
+    var grades = [aplus, a, aminus, bplus, b, bminus, cplus, c, cminus, d, f];
+    var iterB = bounds.length - 1;
+    var iterG = grades.length - 1;
 
-/* const myForm = document.getElementById("upload");
-const selectedFile = document.getElementById("selectedFile"); */
+    console.log(bounds);
+
+    for (var i = data.length - 1; i > -1; --i) { // sort data into grade count
+        if (Number(data[i].Percent) < bounds[iterB - 1]) {
+            console.log("data[", i, "] = ", Number(data[i].Percent));
+            console.log("position", iterG, " + 1");
+            console.log(bounds[iterB]);
+            ++grades[iterG];
+            continue;
+        } else {
+            ++grades[iterG - 1];
+        }
+        --iterB;
+        --iterG;
+    }
+    console.log(grades);
+}
+
+myBounds.addEventListener("keydown", function (e) {
+    if (e.code === "Enter") { // Checks whether "Enter" key is pressed
+        bounds = boundUpdate();
+        console.log(bounds);
+        calcStats(objects);
+        calcGrades(objects);
+        console.log("Stats and histogram updated");
+    }
+})
+
+function init() {
+    document.getElementById("selectedFile").addEventListener('change', handleFileSelect, false);
+}
+
+function handleFileSelect(event) {
+    const reader = new FileReader()
+    reader.onload = handleFileLoad;
+    reader.readAsText(event.target.files[0])
+}
+
+function handleFileLoad(event) {
+    console.log(event);
+    console.log("Submission logged");
+    fileUploaded = true;
+    const text = event.target.result;
+    const data = csvToArray(text);
+
+    function compare(a, b) { // Compare function for sort()
+        if (Number(a.Percent) > Number(b.Percent)) {
+            return -1;
+        }
+        if (Number(a.Percent) < Number(b.Percent)) {
+            return 1;
+        }
+        return 0;
+    }
+    data.sort(compare); // sort data
+    console.log(data);
+    objects = data;
+
+    calcStats(objects);
+    calcGrades(objects);
+    console.log("stats calculated, histogram generated");
+}
 
 function csvToArray(str, delimiter = ",") { // function to parse CSV into array of objects
 
@@ -121,25 +172,7 @@ function csvToArray(str, delimiter = ",") { // function to parse CSV into array 
     return arr1;
 }
 
-/* myForm.addEventListener("submit", function (e) { // reading CSV file
-    e.preventDefault();
-    const input = selectedFile.files[0];
-    const reader = new FileReader();
-    const arr = [];
-    reader.onload = function (e) {
-        const text = e.target.result;
-        const data = csvToArray(text);
-        for (var i = 0; i < data.length; i++) {
-            arr[i] = data[i];
-        }
-    }
-    console.log(arr);
-    console.log("Submission logged");
-    reader.readAsText(input);
+var canvas = document.querySelector("canvas");
 
-
-    arr.sort(compare);
-    console.log(arr);
-    console.log(arr.prototype.pop());
-}); */
-
+var c = canvas.getContext("2d");
+c.fillRect(10, 10, 10, 10);
